699723f7eb962d34cda1bcd84465e7816eb3b9e2
Vinicius Benicio <vinicius.benicio97@gmail.com>
2024-06-17 19:01:57 -0300

    Criando a controller iniicial e adicionando pacote do RabbitMQ

---
+++ GerenciadorLivro.Notification.API/Controllers/NotificacaoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorLivro.Notification.API.Controllers
+{
+    [Route("api/notificacoes")]
+    public class NotificacaoController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+    }
+}
